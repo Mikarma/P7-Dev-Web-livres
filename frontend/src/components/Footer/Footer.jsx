9956e28ee5dc1fe16fbeb37a22fb4681ad9f7608
Mikarma <111996919+Mikarma@users.noreply.github.com>
2026-06-02 17:14:35 +0200

    test validation

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
         <address>
           {'8 place Jeanne d\'Arc'}
           <br />
-          59000 Lille
+          59000 Lille test verification
           <br />
           <a className={styles.MapLink} target="_blank" href={'https://www.google.com/maps/place/8+Pl.+Jeanne+d\'Arc,+59000+Lille,+France/@50.6259112,3.0641085,17z/data=!3m1!4b1!4m5!3m4!1s0x47c2d590f9fc2c2d:0xf0d7614e1aa1964e!8m2!3d50.6259112!4d3.0641085'} rel="noreferrer">
             voir sur la carte
